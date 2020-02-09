3c83f16f0b92f0d0c245fdb72c76362b86b7d179
cheginit <taher.chegini@gmail.com>
2020-02-09 17:54:36 -0600

    removed unused package

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import hydrodata
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
-              'jupyter_sphinx.execute', 'nbsphinx', 'sphinx.ext.mathjax',
+              'nbsphinx', 'sphinx.ext.mathjax',
               'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
