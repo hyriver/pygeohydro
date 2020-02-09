e4f09528b56d1a11b31ae70728c7c4336d9fc242
cheginit <taher.chegini@gmail.com>
2020-02-09 17:48:42 -0600

    Bump version: 0.1.7 → 0.2.0

--- hydrodata/__init__.py
+++ hydrodata/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Taher Chegini"""
 __email__ = "cheginit@gmail.com"
-__version__ = "__version__ = '0.1.7'"
+__version__ = "__version__ = '0.2.0'"
 
 from .hydrodata import Dataloader
