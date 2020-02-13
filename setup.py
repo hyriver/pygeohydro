722c14f56fb4411b9b9d97a61c44e0e2813920fb
cheginit <taher.chegini@gmail.com>
2020-02-13 11:46:34 -0600

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/cheginit/hydrodata",
-    version="version='0.3.0'",
+    version="version='0.3.1'",
     zip_safe=False,
 )
