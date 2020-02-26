83dc2e813ae2786898ed69bfc4031aa26883fdf5
cheginit <taher.chegini@gmail.com>
2020-02-26 17:24:23 -0600

    added bump2version to setup

--- hydrodata/__init__.py
+++ hydrodata/__init__.py
@@ -1,9 +1,8 @@
 """Top-level package for Hydrodata."""
-import pkg_resources
 
 from .hydrodata import Station
 
 __author__ = """Taher Chegini"""
 __email__ = "cheginit@gmail.com"
 
-__version__ = pkg_resources.get_distribution("hydrodata").version
+__version__ = "0.3.0"
