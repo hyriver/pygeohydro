124a923a7a562ba3a480bbb8f57a99b0cd2b3c6e
cheginit <taher.chegini@gmail.com>
2020-06-22 00:39:17 -0600

    add classes to init for easier import

--- hydrodata/__init__.py
+++ hydrodata/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for Hydrodata."""
 
-from .hydrodata import Station
+from .datasets import NLDI, NationalMap, Station, WaterData
+from .services import WFS, ArcGISREST
 
 __author__ = """Taher Chegini"""
 __email__ = "cheginit@gmail.com"
-
 __version__ = "0.5.5"
