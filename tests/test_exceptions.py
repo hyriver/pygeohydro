8ceae0a187208b82edcd317ee4a46435e83a6bb9
cheginit <taher.chegini@gmail.com>
2020-06-28 20:03:23 -0500

    isort

---
+++ tests/test_exceptions.py
@@ -0,0 +1,75 @@
+import pytest
+
+from hydrodata import (
+    InvalidInputRange,
+    InvalidInputType,
+    InvalidInputValue,
+    MissingInputs,
+    MissingItems,
+    ServerError,
+    ZeroMatched,
+)
+
+
+def server_error():
+    url = "https://hydro.nationalmap.gov/arcgis/rest/services/wbd/MapServer/1000"
+    raise ServerError(url)
+
+
+def test_server_error():
+    with pytest.raises(ServerError):
+        server_error()
+
+
+def missing_items():
+    raise MissingItems(["tmin", "dayl"])
+
+
+def test_missing_items():
+    with pytest.raises(MissingItems):
+        missing_items()
+
+
+def zero_matched():
+    raise ZeroMatched("Query returned no matched objects.")
+
+
+def test_zero_matched():
+    with pytest.raises(ZeroMatched):
+        zero_matched()
+
+
+def invalid_value():
+    raise InvalidInputValue("outFormat", ["json", "geojson"])
+
+
+def test_invalid_value():
+    with pytest.raises(InvalidInputValue):
+        invalid_value()
+
+
+def invalid_type():
+    raise InvalidInputType("coords", "tuple", "(lon, lat)")
+
+
+def test_invalid_type():
+    with pytest.raises(InvalidInputType):
+        invalid_type()
+
+
+def invalid_range():
+    raise InvalidInputRange("Input is out of range.")
+
+
+def test_invalid_range():
+    with pytest.raises(InvalidInputRange):
+        invalid_range()
+
+
+def missing_input():
+    raise MissingInputs("Either coords or station_id should be provided.")
+
+
+def test_missing_input():
+    with pytest.raises(MissingInputs):
+        missing_input()
