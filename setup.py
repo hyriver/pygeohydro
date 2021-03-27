089f2f3f35f29d863c5876601b449e9fac0a5136
cheginit <cheginit@gmail.com>
2021-03-27 16:52:09 -0500

    ENH: Move all configs to setup.cfg file except for scm which should remain in setup.py [skip ci]

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python
 """The setup script."""
 from setuptools import setup  # isort:skip
-import distutils.text_file
-from pathlib import Path
 
-
-def parse_requirements(req):
-    """Read requirements file and return it as a list."""
-    return distutils.text_file.TextFile(Path(__file__).with_name(req)).readlines()
-
-
-with open(Path(__file__).with_name("README.rst")) as f:
-    long_description = f.read()
-
-setup(
-    use_scm_version=True,
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    install_requires=parse_requirements("requirements.txt"),
-)
+setup(use_scm_version={"fallback_version": "999"})
