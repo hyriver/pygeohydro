ba4fe678668fbf4558ab633aa78e287b5519b234
cheginit <cheginit@gmail.com>
2021-07-05 12:03:52 -0500

    ENH: Read the deps from setup.cfg instead of hardcoding them [skip ci]

--- pygeohydro/print_versions.py
+++ pygeohydro/print_versions.py
@@ -3,13 +3,16 @@
 The original script is from
 `xarray <https://github.com/pydata/xarray/blob/master/xarray/util/print_versions.py>`__
 """
+import configparser
 import importlib
 import locale
 import os
 import platform
+import re
 import struct
 import subprocess
 import sys
+from pathlib import Path
 from typing import IO, List, Optional, Tuple
 
 
@@ -61,33 +64,14 @@ def get_sys_info() -> List[Tuple[str, Optional[str]]]:
             ("LC_ALL", f'{os.environ.get("LC_ALL", "None")}'),
             ("LANG", f'{os.environ.get("LANG", "None")}'),
             ("LOCALE", ".".join(str(i) for i in locale.getlocale())),
-        ]
+        ],
     )
 
     return blob
 
 
-def netcdf_and_hdf5_versions() -> List[Tuple[str, Optional[str]]]:
-    """Get netcdf and hdf5 versions."""
-    libhdf5_version = None
-    libnetcdf_version = None
-    try:
-        import netCDF4
-
-        libhdf5_version = netCDF4.__hdf5libversion__
-        libnetcdf_version = netCDF4.__netcdf4libversion__
-    except ImportError:
-        try:
-            import h5py
-
-            libhdf5_version = h5py.version.hdf5_version
-        except ImportError:
-            pass
-    return [("libhdf5", libhdf5_version), ("libnetcdf", libnetcdf_version)]
-
-
 def show_versions(file: IO = sys.stdout) -> None:
-    """Print the versions of pygeohydro stack and its dependencies.
+    """Print versions of all the dependencies.
 
     Parameters
     ----------
@@ -95,49 +79,23 @@ def show_versions(file: IO = sys.stdout) -> None:
         print to the given file-like object. Defaults to sys.stdout.
     """
     sys_info = get_sys_info()
-
-    try:
-        sys_info.extend(netcdf_and_hdf5_versions())
-    except OSError as e:
-        print(f"Error collecting netcdf / hdf5 version: {e}")
-
+    config = configparser.ConfigParser()
+    config.read(Path(Path(__file__).parent, "..", "setup.cfg"))
+    req_list = re.sub(r"\[(.*?)\]", "", config.get("options", "install_requires").strip()).split(
+        "\n"
+    )
     deps = [
-        # (MODULE_NAME, f(mod) -> mod version)
-        # pygeohydro stack
-        ("pygeohydro", lambda mod: mod.__version__),
-        ("pygeoogc", lambda mod: mod.__version__),
-        ("pygeoutils", lambda mod: mod.__version__),
-        ("py3dep", lambda mod: mod.__version__),
-        ("pynhd", lambda mod: mod.__version__),
-        ("pydaymet", lambda mod: mod.__version__),
+        # package version
+        (config.get("metadata", "name"), lambda mod: mod.__version__),
         # dependencies
-        ("xarray", lambda mod: mod.__version__),
-        ("rasterio", lambda mod: mod.__version__),
-        ("geopandas", lambda mod: mod.__version__),
-        ("pandas", lambda mod: mod.__version__),
-        ("orjson", lambda mod: mod.__version__),
-        ("shapely", lambda mod: mod.__version__),
-        ("scipy", lambda mod: mod.__version__),
-        ("netCDF4", lambda mod: mod.__version__),
-        ("defusedxml", lambda mod: mod.__version__),
-        ("owslib", lambda mod: mod.__version__),
-        ("pyproj", lambda mod: mod.__version__),
-        ("requests", lambda mod: mod.__version__),
-        ("pygeos", lambda mod: mod.__version__),
-        ("matplotlib", lambda mod: mod.__version__),
-        # optionals
-        ("bottleneck", lambda mod: mod.__version__),
-        ("dask", lambda mod: mod.__version__),
-        ("cartopy", lambda mod: mod.__version__),
+        *((r, lambda mod: mod.__version__) for r in req_list),
         # setup/test
         ("setuptools", lambda mod: mod.__version__),
         ("pip", lambda mod: mod.__version__),
         ("conda", lambda mod: mod.__version__),
         ("mamba", lambda mod: mod.__version__),
         ("pytest", lambda mod: mod.__version__),
-        # Misc.
-        ("IPython", lambda mod: mod.__version__),
-        ("sphinx", lambda mod: mod.__version__),
+        ("ward", lambda mod: mod.__version__),
     ]
 
     deps_blob: List[Tuple[str, Optional[str]]] = []
@@ -146,14 +104,17 @@ def show_versions(file: IO = sys.stdout) -> None:
             if modname in sys.modules:
                 mod = sys.modules[modname]
             else:
-                mod = importlib.import_module(modname)
+                try:
+                    mod = importlib.import_module(modname)
+                except ModuleNotFoundError:
+                    mod = importlib.import_module(modname.replace("-", "_"))
         except ModuleNotFoundError:
             deps_blob.append((modname, None))
         else:
             try:
                 ver = ver_f(mod)
                 deps_blob.append((modname, ver))
-            except NotImplementedError:
+            except (NotImplementedError, AttributeError):
                 deps_blob.append((modname, "installed"))
 
     print("\nINSTALLED VERSIONS", file=file)
