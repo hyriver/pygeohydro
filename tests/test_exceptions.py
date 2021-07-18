f80c69d9feaacbd24b41e30098689ef357510452
cheginit <cheginit@gmail.com>
2021-07-17 23:06:45 -0500

    ENH: Remove the exception tests to be replaced with proper exception tests later

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,30 +1,3 @@
 import pytest
 
 from pygeohydro import InvalidInputRange, InvalidInputType, InvalidInputValue
-
-
-def invalid_value():
-    raise InvalidInputValue("outFormat", ["json", "geojson"])
-
-
-def test_invalid_value():
-    with pytest.raises(InvalidInputValue):
-        invalid_value()
-
-
-def invalid_type():
-    raise InvalidInputType("coords", "tuple", "(lon, lat)")
-
-
-def test_invalid_type():
-    with pytest.raises(InvalidInputType):
-        invalid_type()
-
-
-def invalid_range():
-    raise InvalidInputRange("Input is out of range.")
-
-
-def test_invalid_range():
-    with pytest.raises(InvalidInputRange):
-        invalid_range()
