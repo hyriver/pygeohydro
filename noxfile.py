1eecf858fa4ad40645babb5487be16bc98b3d5a8
cheginit <cheginit@gmail.com>
2021-11-30 15:19:52 -0600

    ENH: Remove files and folders that are generated during the instalation step

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,6 @@
+import shutil
+from pathlib import Path
+
 import nox
 
 
@@ -10,3 +13,12 @@ def tests(session):
     session.run("pytest")
     session.run("coverage", "report")
     session.run("coverage", "html")
+
+    dirs = [".pytest_cache", "build", "dist", ".eggs"]
+    for d in dirs:
+        shutil.rmtree(d, ignore_errors=True)
+
+    patters = ["*.egg-info", "*.egg", "*.pyc", "*~", "**/__pycache__"]
+    for p in patters:
+        for f in Path.cwd().rglob(p):
+            shutil.rmtree(f, ignore_errors=True)
