e43e45f9237ec402aeb5c29152625f0934756f23
cheginit <cheginit@gmail.com>
2021-12-08 16:24:14 -0600

    ENH: Refactor to improve readability and easier dep handling [skip ci]

--- noxfile.py
+++ noxfile.py
@@ -3,17 +3,21 @@ from pathlib import Path
 
 import nox
 
+HR_DEPS = ["async_retriever", "pygeoogc", "pygeoutils", "pynhd"]
+
 
 @nox.session(python="3.9")
 def tests(session):
-    session.install(".[test]")
-    hr_deps = ["async_retriever", "pygeoogc", "pygeoutils", "pynhd"]
-    for p in hr_deps:
-        session.install(f"git+https://github.com/cheginit/{p}.git")
+    install_deps(session)
+
     session.run("pytest")
     session.run("coverage", "report")
     session.run("coverage", "html")
 
+
+def install_deps(session):
+    deps = [".[test]"] + [f"git+https://github.com/cheginit/{p}.git" for p in HR_DEPS]
+    session.install(*deps)
     dirs = [".pytest_cache", "build", "dist", ".eggs"]
     for d in dirs:
         shutil.rmtree(d, ignore_errors=True)
