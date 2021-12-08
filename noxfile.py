f9262fbf8bab4ff88fea87139d3489c2941fab26
cheginit <cheginit@gmail.com>
2021-12-07 21:44:42 -0600

    ENH: Run the local tests with py3.10 [skip ci]

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import nox
 
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def tests(session):
     session.install(".[test]")
     hr_deps = ["async_retriever", "pygeoogc", "pygeoutils", "pynhd"]
