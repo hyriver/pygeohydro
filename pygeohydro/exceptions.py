e1fe6083a71ece014bffd4a2bb2a152eb58ebc0f
cheginit <cheginit@gmail.com>
2021-12-26 15:52:55 -0600

    ENH: Add the new exception class [skip ci]

--- pygeohydro/exceptions.py
+++ pygeohydro/exceptions.py
@@ -4,6 +4,23 @@ from typing import Generator, List, Optional, Tuple, Union
 import pygeoogc as ogc
 
 
+class MissingColumns(Exception):
+    """Exception raised when a required column is missing from a dataframe.
+
+    Parameters
+    ----------
+    missing : list
+        List of missing columns.
+    """
+
+    def __init__(self, missing: List[str]) -> None:
+        self.message = "The following columns are missing:\n" + f"{', '.join(missing)}"
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return self.message
+
+
 class MissingCRS(Exception):
     """Exception raised when input GeoDataFrame is missing CRS."""
 
