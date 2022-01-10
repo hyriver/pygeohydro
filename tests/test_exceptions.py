bae18f4df2e517e5b63a3fc19d21ed9dcc2795cd
cheginit <cheginit@gmail.com>
2022-01-09 23:58:39 -0600

    ENH: Ignore tests with intentional type issues during typeguard testing [skip ci]

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,6 +4,13 @@ from shapely.geometry import Polygon
 import pygeohydro as gh
 from pygeohydro import DataNotAvailable, InvalidInputRange, InvalidInputType, InvalidInputValue
 
+try:
+    import typeguard  # noqa: F401
+except ImportError:
+    has_typeguard = False
+else:
+    has_typeguard = True
+
 SID_NATURAL = "01031500"
 GEOM = Polygon(
     [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
@@ -14,16 +21,19 @@ class TestETAExceptions:
     "Test ssebopeta Exceptions"
     dates = ("2000-01-01", "2000-01-05")
 
+    @pytest.mark.skipif(has_typeguard, reason="Broken if Typeguard is enabled")
     def test_invalid_coords(self):
         with pytest.raises(InvalidInputType) as ex:
             _ = gh.ssebopeta_byloc(GEOM.centroid.x, dates=self.dates)
         assert "(lon, lat)" in str(ex.value)
 
+    @pytest.mark.skipif(has_typeguard, reason="Broken if Typeguard is enabled")
     def test_invalid_dates(self):
         with pytest.raises(InvalidInputType) as ex:
             _ = gh.ssebopeta_byloc((GEOM.centroid.x, GEOM.centroid.y), dates="2000-01-01")
         assert "tuple" in str(ex.value)
 
+    @pytest.mark.skipif(has_typeguard, reason="Broken if Typeguard is enabled")
     def test_invalid_dates_tuple(self):
         with pytest.raises(InvalidInputType) as ex:
             _ = gh.ssebopeta_byloc((GEOM.centroid.x, GEOM.centroid.y), dates=("2000-01-01"))
@@ -48,6 +58,7 @@ class TestNLCDExceptions:
     res = 1e3
     lulc = gh.nlcd(GEOM, years={"cover": [2016, 2019]}, resolution=1e3, crs="epsg:3542")
 
+    @pytest.mark.skipif(has_typeguard, reason="Broken if Typeguard is enabled")
     def test_invalid_years_type(self):
         with pytest.raises(InvalidInputType) as ex:
             _ = gh.nlcd(GEOM, years=2010, resolution=self.res)
@@ -63,6 +74,7 @@ class TestNLCDExceptions:
             _ = gh.nlcd(GEOM, years={"cover": 2020}, resolution=self.res)
         assert "2019" in str(ex.value)
 
+    @pytest.mark.skipif(has_typeguard, reason="Broken if Typeguard is enabled")
     def test_invalid_cover_type(self):
         with pytest.raises(InvalidInputType) as ex:
             _ = gh.cover_statistics(self.lulc)
