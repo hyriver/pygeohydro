a39397ab654e63539bbc1cf1b3dc18271e6e46b0
cheginit <cheginit@gmail.com>
2022-02-20 14:35:41 -0600

    ENH: Modify NLCD tests to use the new ssl flag

--- tests/test_pygeohydro.py
+++ tests/test_pygeohydro.py
@@ -113,23 +113,23 @@ class TestNLCD:
 
     def test_geodf(self):
         geom = gpd.GeoSeries([GEOM, GEOM], crs=DEF_CRS)
-        lulc = gh.nlcd_bygeom(geom, years=self.years, resolution=self.res, crs=ALT_CRS)
+        lulc = gh.nlcd_bygeom(geom, years=self.years, resolution=self.res, crs=ALT_CRS, ssl=False)
         self.assertion(lulc[0].cover_2016, 84.357)
         self.assertion(lulc[1].cover_2016, 84.357)
 
     def test_coords(self):
         coords = list(GEOM.exterior.coords)
-        lulc = gh.nlcd_bycoords(coords)
+        lulc = gh.nlcd_bycoords(coords, ssl=False)
         assert lulc.cover_2019.sum() == 211
 
     def test_consistency(self):
         coords = [(-87.11890, 34.70421), (-88.83390, 40.17190), (-95.68978, 38.23926)]
-        lulc_m = gh.nlcd_bycoords(coords)
-        lulc_s = gh.nlcd_bycoords(coords[:1])
+        lulc_m = gh.nlcd_bycoords(coords, ssl=False)
+        lulc_s = gh.nlcd_bycoords(coords[:1], ssl=False)
         assert lulc_m.iloc[0]["cover_2019"] == lulc_s.iloc[0]["cover_2019"] == 24
 
     def test_nlcd_deprecated(self):
-        lulc = gh.nlcd(GEOM, years=self.years, resolution=self.res)
+        lulc = gh.nlcd(GEOM, years=self.years, resolution=self.res, ssl=False)
         self.assertion(lulc.cover_2016, 83.4)
 
     @staticmethod
