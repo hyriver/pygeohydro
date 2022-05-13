9832c35b222e2d0b2c6b3189ea39e5a6f896c066
cheginit <cheginit@gmail.com>
2022-05-12 23:08:02 -0500

    ENH: Refactor the plot function to use proplot instead of matplotlib [skip ci]

--- pygeohydro/plot.py
+++ pygeohydro/plot.py
@@ -6,43 +6,76 @@ curve (monthly mean) and flow duration curve.
 import calendar
 import contextlib
 from pathlib import Path
-from typing import Dict, List, NamedTuple, Optional, Tuple, Union
+from typing import Dict, List, NamedTuple, Optional, Tuple, TypeVar, Union
 
-import matplotlib
-import matplotlib.pyplot as plt
 import pandas as pd
+import proplot as pplt
 from matplotlib.colors import BoundaryNorm, ListedColormap
 
 from . import helpers
 from .exceptions import InvalidInputType
+
+__all__ = ["signatures", "prepare_plot_data", "exceedance", "mean_monthly"]
+DF = TypeVar("DF", pd.DataFrame, pd.Series)
+pplt.rc["figure.facecolor"] = "w"
 
 
 class PlotDataType(NamedTuple):
     """Data structure for plotting hydrologic signatures."""
 
     daily: pd.DataFrame
-    monthly: pd.DataFrame
-    annual: pd.DataFrame
     mean_monthly: pd.DataFrame
     ranked: pd.DataFrame
-    bar_width: Dict[str, int]
     titles: Dict[str, str]
     units: Dict[str, str]
 
 
-def exceedance(daily: Union[pd.DataFrame, pd.Series]) -> Union[pd.DataFrame, pd.Series]:
-    """Compute Flow duration (rank, sorted obs)."""
-    if isinstance(daily, pd.Series):
-        daily = daily.to_frame()
+def exceedance(daily: DF, threshold: float = 1e-3) -> DF:
+    """Compute exceedance probability from daily data.
 
-    ranks = daily.rank(ascending=False, pct=True) * 100
+    Parameters
+    ----------
+    daily : pandas.Series or pandas.DataFrame
+        The data to be processed
+    threshold : float, optional
+        The threshold to compute exceedance probability, defaults to 1e-3.
+
+    Returns
+    -------
+    pandas.Series or pandas.DataFrame
+        Exceedance probability.
+    """
+    _daily = daily[daily > threshold].copy()
+    ranks = _daily.rank(ascending=False, pct=True) * 100
     fdc = [
-        pd.DataFrame({c: daily[c], f"{c}_rank": ranks[c]})
+        pd.DataFrame({c: _daily[c], f"{c}_rank": ranks[c]})
         .sort_values(by=f"{c}_rank")
         .reset_index(drop=True)
         for c in daily
     ]
     return pd.concat(fdc, axis=1)
+
+
+def mean_monthly(daily: DF, index_abbr: bool = False) -> DF:
+    """Compute mean monthly summary from daily data.
+
+    Parameters
+    ----------
+    daily : pandas.Series or pandas.DataFrame
+        The data to be processed
+
+    Returns
+    -------
+    pandas.Series or pandas.DataFrame
+        Mean monthly summary.
+    """
+    monthly = daily.groupby(pd.Grouper(freq="M")).sum()
+    mean_month = monthly.groupby(monthly.index.month).mean()
+    mean_month.index.name = "Month"
+    if index_abbr:
+        month_abbr = dict(enumerate(calendar.month_abbr))
+        mean_month.index = mean_month.index.map(month_abbr)
+    return mean_month
 
 
 def prepare_plot_data(daily: Union[pd.DataFrame, pd.Series]) -> PlotDataType:
@@ -52,45 +85,33 @@ def prepare_plot_data(daily: Union[pd.DataFrame, pd.Series]) -> PlotDataType:
     ----------
     daily : pandas.Series or pandas.DataFrame
         The data to be processed
+    ranked : bool, optional
+        Whether to sort the data by rank for plotting flow duration curve, defaults to False.
 
     Returns
     -------
-    PlotDataType
-        Containing ``daily, ``monthly``, ``annual``, ``mean_monthly``, ``ranked`` fields.
+    NamedTuple
+        Containing ``daily, ``mean_monthly``, ``ranked`` fields.
     """
     if isinstance(daily, pd.Series):
         daily = daily.to_frame()
-    daily.index = daily.index.tz_localize(None)
-
-    monthly = daily.groupby(pd.Grouper(freq="M")).sum()
-
-    annual = daily.groupby(pd.Grouper(freq="Y")).sum()
-
-    month_abbr = dict(enumerate(calendar.month_abbr))
-    mean_month = daily.groupby(pd.Grouper(freq="M")).sum()
-    mean_month = mean_month.groupby(mean_month.index.month).mean()
-    mean_month.index = mean_month.index.map(month_abbr)
-
+    mean_month = mean_monthly(daily, True)
     ranked = exceedance(daily)
+
     _titles = [
         "Total Hydrograph (daily)",
-        "Total Hydrograph (monthly)",
-        "Total Hydrograph (annual)",
         "Regime Curve (monthly mean)",
         "Flow Duration Curve",
     ]
     _units = [
         "mm/day",
         "mm/month",
-        "mm/year",
-        "mm/month",
         "mm/day",
     ]
     fields = PlotDataType._fields
     titles = dict(zip(fields[:-1], _titles))
     units = dict(zip(fields[:-1], _units))
-    bar_width = dict(zip(fields[:-2], [1, 30, 365, 1]))
-    return PlotDataType(daily, monthly, annual, mean_month, ranked, bar_width, titles, units)
+    return PlotDataType(daily, mean_month, ranked, titles, units)
 
 
 def _prepare_plot_data(
@@ -111,20 +132,18 @@ def _prepare_plot_data(
 
 
 def signatures(
-    daily: Union[pd.DataFrame, pd.Series],
+    discharge: Union[pd.DataFrame, pd.Series],
     precipitation: Optional[pd.Series] = None,
     title: Optional[str] = None,
-    title_ypos: float = 1.02,
-    figsize: Tuple[int, int] = (14, 13),
     threshold: float = 1e-3,
-    output: Optional[Union[str, Path]] = None,
+    output: Union[str, Path, None] = None,
+    close: bool = False,
 ) -> None:
     """Plot hydrological signatures with w/ or w/o precipitation.
 
-    Plots includes daily, monthly and annual hydrograph as well as
-    regime curve (mean monthly) and flow duration curve. The input
-    discharges are converted from cms to mm/day based on the watershed
-    area, if provided.
+    Plots includes daily hydrograph, regime curve (mean monthly) and
+    flow duration curve. The input discharges are converted from cms
+    to mm/day based on the watershed area, if provided.
 
     Parameters
     ----------
@@ -136,86 +155,66 @@ def signatures(
         plotted on the second x-axis at the top.
     title : str, optional
         The plot supertitle.
-    title_ypos : float
-        The vertical position of the plot title, default to 1.02
-    figsize : tuple, optional
-        Width and height of the plot in inches, defaults to (14, 13) inches.
     threshold : float, optional
         The threshold for cutting off the discharge for the flow duration
         curve to deal with log 0 issue, defaults to :math:`1^{-3}` mm/day.
     output : str, optional
         Path to save the plot as png, defaults to ``None`` which means
         the plot is not saved to a file.
+    close : bool, optional
+        Whether to close the figure.
     """
-    discharge, prcp = _prepare_plot_data(daily, precipitation)
+    qdaily, prcp = _prepare_plot_data(discharge, precipitation)
 
-    fig = plt.figure(figsize=figsize)
-    gs = fig.add_gridspec(4, 2)
-    sub_ax = [gs[0, :], gs[1, :], gs[2, 0], gs[2, 1], gs[3, :]]
+    cycle = "colorblind10"
+    fig, axs = pplt.subplots(
+        [[1, 1, 1, 1], [2, 2, 3, 3]], refwidth=6.5, refheight=2.2, share=0, facecolor="w"
+    )
+    axs.format(titleloc="uc", toplabels=(title,), lw=0.7)
 
-    for sp, f in zip(sub_ax[:-1], discharge._fields[:-2]):
-        ax = fig.add_subplot(sp)
-        _discharge = getattr(discharge, f)  # noqa: B009
-        _title = discharge.titles[f]
-        _unit = discharge.units[f]
-        qxval = _discharge.index
+    daily = qdaily.daily
+    fdc = qdaily.ranked
+    rc = qdaily.mean_monthly
 
-        ax.plot(qxval, _discharge.to_numpy())
-        ax.set_ylabel(f"$Q$ ({_unit})")
+    hs = axs[0, 0].plot(daily, cycle=cycle)
+    axs[0, 0].format(xlabel="", ylabel=f"$Q$ ({qdaily.units['daily']})", xrotation=0)
+    axs[0, 0].margins(x=0)
 
-        if prcp is not None:
-            _prcp = getattr(prcp, f)  # noqa: B009
-            _prcp = _prcp.loc[_prcp.index.intersection(qxval)]
+    if len(hs) > 1:
+        fig.legend(hs, ncols=1, frame=False, loc="r")
 
-            ax_p = ax.twinx()
-            if _prcp.shape[0] > 1000:
-                ax_p.plot(_prcp.index, _prcp.to_numpy().ravel(), alpha=0.7, color="g")
-            else:
-                ax_p.bar(
-                    _prcp.index,
-                    _prcp.to_numpy().ravel(),
-                    alpha=0.7,
-                    width=prcp.bar_width[f],
-                    color="g",
-                    align="edge",
-                )
-            ax_p.set_ylim(_prcp.max().to_numpy()[0] * 2.5, 0)
-            ax_p.set_ylabel(f"$P$ ({_unit})")
+    axs[1, 0].plot(rc, cycle=cycle)
+    axs[1, 0].format(xlabel="", ylabel=f"$Q$ ({qdaily.units['mean_monthly']})")
 
-        ax.set_xlim(qxval[0], qxval[-1])
-        ax.set_xlabel("")
-        ax.set_title(_title)
+    if prcp is not None:
+        ox = axs[0, 0].alty(reverse=True, label=f"$P$ ({prcp.units['daily']})")
+        ox.bar(prcp.daily, color="g", alpha=0.2, width=1)
+        ox.margins(x=0)
 
-        if len(_discharge.columns) > 1 and f == "daily":
-            ax.legend(
-                _discharge.columns,
-                bbox_to_anchor=(0.0, 1.02, 1.0, 0.102),
-                loc="lower right",
-                ncol=len(_discharge),
-            )
+        ox = axs[1, 0].alty(
+            reverse=True,
+            lim=(0, prcp.mean_monthly.max()[0] * 2.5),
+            label=f"$P$ ({prcp.units['mean_monthly']})",
+        )
+        ox.bar(prcp.mean_monthly, color="g", alpha=0.2, width=1)
 
-        if f == "annual":
-            ax.xaxis.set_major_locator(matplotlib.dates.YearLocator())
-            ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y"))
-
-    ax = fig.add_subplot(sub_ax[-1])
-    for col in discharge.daily:
-        dc = discharge.ranked[[col, f"{col}_rank"]]
-        dc = dc[dc > threshold]
-        ax.plot(dc[f"{col}_rank"], dc[col], label=col)
-
-    ax.set_yscale("log")
-    ax.set_xlim(0, 100)
-    ax.set_xlabel("% Exceedance")
-    ax.set_ylabel(rf"$\log(Q)$ ({discharge.units['ranked']})")
-    ax.set_title("Flow Duration Curve")
-
-    plt.tight_layout()
-    plt.suptitle(title, size=16, y=title_ypos)
+    for col in daily:
+        _fdc = fdc[[col, f"{col}_rank"]]
+        _fdc = _fdc[_fdc > threshold]
+        axs[1, 2].plot(_fdc[f"{col}_rank"], _fdc[col], cycle=cycle)
+    logq = "$\\log \\left( Q \\right)$"
+    axs[1, 2].format(
+        yscale="symlog",
+        xlabel="Exceedance Probability",
+        ylabel=f"{logq} ({qdaily.units['ranked']})",
+    )
 
     if output is not None:
-        Path(output).parent.mkdir(parents=True, exist_ok=True)
-        plt.savefig(output, dpi=300, bbox_inches="tight")
+        Path(output).parent.mkdir(exist_ok=True, parents=True)
+        fig.savefig(output, dpi=300)
+
+    if close:
+        pplt.close(fig)
 
 
 def descriptor_legends() -> Tuple[ListedColormap, BoundaryNorm, List[int]]:
