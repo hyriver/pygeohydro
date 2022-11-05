2826693d29267a7a45e7a4031960ba4104b88b2b
cheginit <cheginit@gmail.com>
2022-11-04 19:59:11 -0500

    ENH: Add stac deps [skip ci]

--- noxfile.py
+++ noxfile.py
@@ -109,7 +109,7 @@ def pre_commit(session) -> None:
 @nox.session(python=python_versions)
 def tests(session):
     """Run the test suite."""
-    install_deps(session, "test")
+    install_deps(session, "test,stac")
 
     session.run("pytest", "--doctest-modules", *session.posargs)
     session.run("coverage", "report")
