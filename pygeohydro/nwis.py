e18a9305c249d31f87f86f77fd12605f52b7406c
Taher Chegini <cheginit@gmail.com>
2023-05-23 10:01:51 -0500

    BUG: When the corresponding nhdplus IDs of the stations are not available in the nhdplus anymore, return NA instread of failing. [skip ci]

--- pygeohydro/nwis.py
+++ pygeohydro/nwis.py
@@ -289,8 +289,8 @@ class NWIS:
             area["comid"] = area["comid"].astype("int32")
         except (TypeError, IntCastingNaNError):
             area["comid"] = area["comid"].astype("Int32")
-        nhd_area = pynhd.streamcat("fert", comids=area["comid"].to_list())
-        area = area.merge(nhd_area[["COMID", "WSAREASQKM"]], left_on="comid", right_on="COMID")
+        nhd_area = pynhd.streamcat("fert", comids=area["comid"].dropna().to_list())
+        area = area.merge(nhd_area[["COMID", "WSAREASQKM"]], left_on="comid", right_on="COMID", how="left")
         area["identifier"] = area["identifier"].str.replace("USGS-", "")
         area = area[["identifier", "comid", "reachcode", "measure", "WSAREASQKM"]].copy()
         area = area.rename(
