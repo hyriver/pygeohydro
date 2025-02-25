1ee7f109278860d958a0ccce7482cfb7c104b341
Taher Chegini <cheginit@gmail.com>
2025-02-24 20:38:36 -0500

    ENH: Move pygeohydro's signature plot to hydrosignatures and make matplotlib and folium an optional deps. [skip ci]

--- src/pygeohydro/plot.py
+++ src/pygeohydro/plot.py
@@ -7,220 +7,32 @@ curve (monthly mean) and flow duration curve.
 from __future__ import annotations
 
 import contextlib
-from dataclasses import dataclass, fields
-from pathlib import Path
 from typing import TYPE_CHECKING, Any, TypeVar
 
-import folium
-import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.colors import BoundaryNorm, ListedColormap
-
-import hydrosignatures as hs
 import pygeoutils as geoutils
 from pygeohydro import helpers
-from pygeohydro.exceptions import InputTypeError
 from pygeohydro.nwis import NWIS
 
 if TYPE_CHECKING:
+    import pandas as pd
+    from folium import Map
+    from matplotlib.colors import BoundaryNorm, ListedColormap
     from pyproj import CRS
 
     CRSType = int | str | CRS
     DF = TypeVar("DF", pd.DataFrame, pd.Series)
 
-__all__ = ["prepare_plot_data", "signatures"]
-
-
-@dataclass(frozen=True)
-class PlotDataType:
-    """Data structure for plotting hydrologic signatures."""
-
-    daily: pd.DataFrame
-    mean_monthly: pd.DataFrame
-    ranked: pd.DataFrame
-    titles: dict[str, str]
-    units: dict[str, str]
-
-    @classmethod
-    def fields(cls) -> tuple[str, ...]:
-        """Return the field names of the dataclass."""
-        return tuple(field.name for field in fields(cls))
-
-
-def prepare_plot_data(daily: pd.DataFrame | pd.Series) -> PlotDataType:
-    """Generate a structured data for plotting hydrologic signatures.
-
-    Parameters
-    ----------
-    daily : pandas.Series or pandas.DataFrame
-        The data to be processed
-
-    Returns
-    -------
-    PlotDataType
-        Containing ``daily, ``mean_monthly``, ``ranked``, ``titles``,
-        and ``units`` fields.
-    """
-    if isinstance(daily, pd.Series):
-        daily = daily.to_frame()
-    mean_month = hs.mean_monthly(daily, True)
-    ranked = hs.exceedance(daily)
-
-    _titles = [
-        "Total Hydrograph (daily)",
-        "Regime Curve (monthly mean)",
-        "Flow Duration Curve",
-    ]
-    _units = [
-        "mm/day",
-        "mm/month",
-        "mm/day",
-    ]
-    _fields = PlotDataType.fields()
-    titles = dict(zip(_fields[:-1], _titles))
-    units = dict(zip(_fields[:-1], _units))
-    return PlotDataType(daily, mean_month, ranked, titles, units)
-
-
-def _prepare_plot_data(
-    daily: pd.DataFrame | pd.Series,
-    precipitation: pd.DataFrame | pd.Series | None = None,
-) -> tuple[PlotDataType, PlotDataType | None]:
-    if not isinstance(daily, (pd.DataFrame, pd.Series)):
-        raise InputTypeError("daily", "pandas.DataFrame or pandas.Series")
-
-    discharge = prepare_plot_data(daily)
-    if precipitation is not None:
-        if isinstance(precipitation, pd.DataFrame) and precipitation.shape[1] == 1:
-            prcp = prepare_plot_data(precipitation.squeeze())  # pyright: ignore[reportArgumentType]
-        elif isinstance(precipitation, pd.Series):
-            prcp = prepare_plot_data(precipitation)
-        else:
-            raise InputTypeError(
-                "precipitation", "pandas.Series or pandas.DataFrame with one column"
-            )
-    else:
-        prcp = None
-
-    return discharge, prcp
-
-
-def signatures(
-    discharge: pd.DataFrame | pd.Series,
-    precipitation: pd.Series | None = None,
-    title: str | None = None,
-    figsize: tuple[int, int] | None = None,
-    output: str | Path | None = None,
-    close: bool = False,
-) -> None:
-    """Plot hydrological signatures w/ or w/o precipitation.
-
-    Plots includes daily hydrograph, regime curve (mean monthly) and
-    flow duration curve. The input discharges are converted from cms
-    to mm/day based on the watershed area, if provided.
-
-    Parameters
-    ----------
-    discharge : pd.DataFrame or pd.Series
-        The streamflows in mm/day. The column names are used as labels
-        on the plot and the column values should be daily streamflow.
-    precipitation : pd.Series, optional
-        Daily precipitation time series in mm/day. If given, the data is
-        plotted on the second x-axis at the top.
-    title : str, optional
-        The plot supertitle.
-    figsize : tuple, optional
-        The figure size in inches, defaults to (9, 5).
-    output : str, optional
-        Path to save the plot as png, defaults to ``None`` which means
-        the plot is not saved to a file.
-    close : bool, optional
-        Whether to close the figure.
-    """
-    qdaily, prcp = _prepare_plot_data(discharge, precipitation)
-
-    daily = qdaily.daily
-    figsize = (9, 5) if figsize is None else figsize
-    fig = plt.figure(constrained_layout=True, figsize=figsize, facecolor="w")
-    gs = fig.add_gridspec(2, 3)
-
-    ax = fig.add_subplot(gs[0, :])
-    for c, q in daily.items():
-        ax.plot(q, label=c)
-    lines, labels = ax.get_legend_handles_labels()
-    ax.set_ylabel("$Q$ (mm/day)")
-    ax.text(0.02, 0.9, "(a)", transform=ax.transAxes, ha="left", va="center", fontweight="bold")
-
-    if prcp is not None:
-        _prcp = prcp.daily.squeeze()
-        _prcp = _prcp.loc[daily.index[0] : daily.index[-1]]
-        label = "$P$ (mm/day)"
-        ax_p = ax.twinx()
-        ax_p.grid(False)
-        if _prcp.shape[0] > 1000:
-            ax_p.plot(
-                _prcp,
-                alpha=0.7,
-                color="g",
-                label=label,
-            )
-        else:
-            ax_p.bar(
-                _prcp.index,
-                _prcp.to_numpy().ravel(),
-                alpha=0.7,
-                width=1,
-                color="g",
-                align="edge",
-                label=label,
-            )
-        ax_p.set_ylim(_prcp.max() * 2.5, 0)
-        ax_p.set_ylabel(label)
-        ax_p.set_xmargin(0)
-        lines_p, labels_p = ax_p.get_legend_handles_labels()
-        lines.extend(lines_p)
-        labels.extend(labels_p)
-
-    ax.set_xmargin(0)
-    ax.set_xlabel("")
-    ax.legend(
-        lines,
-        labels,
-        bbox_to_anchor=(0.5, -0.2),
-        loc="upper center",
-        ncol=len(lines),
-    )
-
-    ax = fig.add_subplot(gs[1, :-1])
-    ax.plot(qdaily.mean_monthly)
-    ax.set_xmargin(0)
-    ax.set_ylabel("$Q$ (mm/month)")
-    ax.text(0.02, 0.9, "(b)", transform=ax.transAxes, ha="left", va="center", fontweight="bold")
-
-    ax = fig.add_subplot(gs[1, 2])
-    for col in daily:
-        dc = qdaily.ranked[[col, f"{col}_rank"]]
-        ax.plot(dc[f"{col}_rank"], dc[col], label=col)
-
-    ax.set_yscale("log")
-    ax.set_xlim(0, 100)
-    ax.set_xlabel("% Exceedance")
-    ax.set_ylabel(rf"$\log(Q)$ ({qdaily.units['ranked']})")
-    ax.text(0.02, 0.9, "(c)", transform=ax.transAxes, ha="left", va="center", fontweight="bold")
-
-    if title:
-        fig.suptitle(title)
-
-    if output is not None:
-        Path(output).parent.mkdir(exist_ok=True, parents=True)
-        fig.savefig(output, dpi=300)  # pyright: ignore[reportGeneralTypeIssues]
-
-    if close:
-        plt.close(fig)
+__all__ = ["cover_legends", "descriptor_legends", "interactive_map"]
 
 
 def descriptor_legends() -> tuple[ListedColormap, BoundaryNorm, list[int]]:
     """Colormap (cmap) and their respective values (norm) for land cover data legends."""
+    try:
+        from matplotlib.colors import BoundaryNorm, ListedColormap
+    except ImportError as e:
+        raise ImportError(
+            "matplotlib is required for descriptor legends. Please install it."
+        ) from e
     nlcd_meta = helpers.nlcd_helper()
     bounds = [int(v) for v in nlcd_meta["descriptors"]]
     with contextlib.suppress(ValueError):
@@ -234,6 +46,12 @@ def descriptor_legends() -> tuple[ListedColormap, BoundaryNorm, list[int]]:
 
 def cover_legends() -> tuple[ListedColormap, BoundaryNorm, list[int]]:
     """Colormap (cmap) and their respective values (norm) for land cover data legends."""
+    try:
+        from matplotlib.colors import BoundaryNorm, ListedColormap
+    except ImportError as e:
+        raise ImportError(
+            "matplotlib is required for descriptor legends. Please install it."
+        ) from e
     nlcd_meta = helpers.nlcd_helper()
     bounds = list(nlcd_meta["colors"])
     with contextlib.suppress(ValueError):
@@ -249,7 +67,7 @@ def interactive_map(
     bbox: tuple[float, float, float, float],
     crs: CRSType = 4326,
     nwis_kwds: dict[str, Any] | None = None,
-) -> folium.Map:
+) -> Map:
     """Generate an interactive map including all USGS stations within a bounding box.
 
     Parameters
@@ -277,6 +95,10 @@ def interactive_map(
     >>> n_stations
     10
     """
+    try:
+        import folium
+    except ImportError as e:
+        raise ImportError("folium is required for interactive map. Please install it.") from e
     nwis = NWIS()
     bbox = geoutils.geometry_reproject(bbox, crs, 4326)
     query = {"bBox": ",".join(f"{b:.06f}" for b in bbox)}
